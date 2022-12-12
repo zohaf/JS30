0ea6cbbef3958a4769667c5a2a16d894028cf96a
zohaf <zohafard@gmail.com>
2022-12-12 12:05:14 +0300

    day 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,77 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+	navigator.mediaDevices
+		.getUserMedia({ video: true, audio: false })
+		.then(localMediaStream => {
+			video.srcObject = localMediaStream;
+			video.play();
+		})
+		.catch(err => console.log(err));
+}
+
+function paintToCanvas() {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+		let pixels = ctx.getImageData(0, 0, width, height);
+
+		pixels = redEffect(pixels);
+
+		//pixels = rgbSplit(pixels);
+		ctx.globalAlpha = 0.8;
+
+		// pixels = greenScreen(pixels);
+		// put them back
+		ctx.putImageData(pixels, 0, 0);
+	}, 16);
+}
+
+function takePhoto() {
+	snap.currentTime = 0;
+	snap.play();
+	//take the data out of the canvas
+	data = canvas.toDataURL('image/jpeg');
+	const link = document.createElement('a');
+	link.href = data;
+	link.setAttribute('download', 'handsome');
+	link.innerHTML = `<image src=${data} alt='handsome man'/>`;
+	strip.insertBefore(link, strip.firstChild);
+	console.log(data);
+}
+
+function redEffect(pixels) {
+	for (let i = 0; i < pixels.data.length; i += 4) {
+		pixels.data[i + 0] = pixels.data[i + 0] + 200;
+		pixels.data[i + 1] = pixels.data[i + 1] + 50;
+		pixels.data[i + 2] = pixels.data[i + 2] - 100;
+	}
+	return pixels;
+}
+
+function rgbSplit(pixels) {
+	for (let i = 0; i < pixels.data.length; i += 4) {
+		pixels.data[i - 150] = pixels.data[i + 0]; // RED
+		pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+		pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+	}
+	return pixels;
+}
+
+function greenScreen() {
+	for (let i = 0; i < pixels.data.length; i += 4) {
+		pixels.data[i - 150] = pixels.data[i + 0]; // RED
+		pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+		pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+	}
+	return pixels;
+}
+
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
